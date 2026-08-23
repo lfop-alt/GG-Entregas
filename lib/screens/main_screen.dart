e5c9aa4ce2775fe05127cd56fcdf0085ba24005b
lfop-alt <lfpedro@outlook.com.br>
2026-08-23 19:21:41 -0300

    Adicionado o repositorio e criado a função que salva a rota no banco de dados atráves do botão da tela inicial Novo Lançamento

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gg_entregas/widgets/app_bar_home.dart';
 import 'package:gg_entregas/widgets/app_bar_widget.dart';
 import 'package:gg_entregas/widgets/navigationbar_floating.dart';
+import 'package:gg_entregas/widgets/show_modal_bottom_custom.dart';
 import 'package:go_router/go_router.dart';
 
 class MainScreen extends StatelessWidget {
@@ -19,14 +20,7 @@ class MainScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Color.fromARGB(255, 7, 53, 65),
         label: Text('Novo Lançamento', style: TextStyle(color: Colors.white)),
-        onPressed: () => showModalBottomSheet(
-          context: context,
-          isScrollControlled: true,
-          builder: (context) => Container(
-            height: MediaQuery.of(context).size.height * 0.8,
-            child: child,
-          ),
-        ),
+        onPressed: () => showModalBottomSheetCustom(context),
         icon: Icon(Icons.add, color: Colors.white),
       ),
     );
